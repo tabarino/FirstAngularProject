c6854912e56b9a222041b2cc237b4c70ee04d252
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-01-24 18:57:32 +0000

    32. Components

---
+++ src/app/courses.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Create by Tabarino
+ * User: itabarino
+ * Date: 24/01/20
+ * Time: 18:40
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses', // <courses>
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+
+}
