f04ba17df76c208f0f4ac7962936314705a51a77
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-01-28 22:25:46 +0000

    34. Templates

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -9,8 +9,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // <courses>
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ "Title: " + title }}</h2><p>{{ getTitle() }}</p>'
 })
 export class CoursesComponent {
+    title = "List of courses";
 
+    getTitle() {
+        return this.title;
+    }
 }
