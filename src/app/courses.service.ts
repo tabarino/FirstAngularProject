adebb0165f45a812831c7f5596c5298ac3daa138
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-01-28 22:54:11 +0000

    36. Services

---
+++ src/app/courses.service.ts
@@ -0,0 +1,12 @@
+/**
+ * Create by Tabarino
+ * User: itabarino
+ * Date: 28/01/20
+ * Time: 22:50
+ */
+
+export class CoursesService {
+    getCourses() {
+        return ["course1", "course2", "course3"];
+    }
+}
