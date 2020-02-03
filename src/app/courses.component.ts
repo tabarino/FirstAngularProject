376e1570025f7e566269a585e8edab78578254ec
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-02-03 22:13:57 +0000

    42. Adding Bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -24,6 +24,7 @@ import { CoursesService } from './courses.service';
                 <td [attr.colspan]="colSpan"></td>
             </tr>
         </table>
+        <button class="btn btn-primary">Save</button>
     `
 })
 export class CoursesComponent {
