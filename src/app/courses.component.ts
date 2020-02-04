434d4915a170e3dfc87c4677a4a6c2a56ce7030e
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-02-04 23:39:12 +0000

    49. Pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -26,7 +26,17 @@ import { CoursesService } from './courses.service';
                 </tr>
             </table>
 
+            <br>
+            <p>{{ course.title | uppercase }}</p>
+            <p>{{ course.students | number }}</p>
+            <p>{{ course.rating | number:'1.2-2' }}</p>
+            <p>{{ course.price | currency:'EUR':true:'3.2-2' }}</p>
+            <p>{{ course.releaseDate| date:'mediumDate' }}</p>
+            <br>
+
             <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+
+            <br><br>
 
             <div (click)="onDivClicked()">
                 <button (click)="onSave($event)" class="btn btn-primary" [class.active]="isActive" [style.backgroundColor]="isActiveBackground ? 'blue' : 'gray'">Save</button>
@@ -42,6 +52,13 @@ export class CoursesComponent {
     isActiveBackground = true;
     email = 'me@example.com';
     courses;
+    course = {
+        title: "The Complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016, 3, 1)
+    }
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
