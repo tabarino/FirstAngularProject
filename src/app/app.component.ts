4d5433a24e123ca73917e80a1f2aeed681d5acd9
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-02-16 22:35:49 +0000

    67. ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,12 @@ export class AppComponent {
 
   viewMode = 'somethingElse';
 
+  ngForCourses = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' }
+  ];
+
   onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
     console.log('Favorite Changed: ', eventArgs);
   }
