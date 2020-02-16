da3be538f717894f71ef44ca1c189e8dd148d273
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-02-16 11:49:34 +0000

    53: Input Properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'First Angular Project';
   testBox: string;
+  post = {
+    favoriteTitle: "Title",
+    isFavorite: true
+  };
 }
