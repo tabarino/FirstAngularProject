85b0d64c1c92bc36062d382104eab44593373365
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-03-22 23:03:20 +0000

    115. Handling Expected Errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -12,9 +12,11 @@ export class PostsComponent implements OnInit {
   constructor(private postService: PostService) { }
 
   ngOnInit(): void {
-    this.postService.getPosts().subscribe(response => {
+    this.postService.getPosts().subscribe(
+    response => {
       this.posts = response;
-    }, error => {
+    },
+    error => {
       alert('An unexpected error occurred');
       console.log(error);
     });
@@ -24,31 +26,46 @@ export class PostsComponent implements OnInit {
     let post: any = { title: input.value };
     input.value = '';
     
-    this.postService.createPost(post).subscribe(response => {
+    this.postService.createPost(post).subscribe(
+    response => {
       post.id = response['id'];
       this.posts.splice(0, 0, post);
-    }, error => {
-      alert('An unexpected error occurred');
-      console.log(error);
+    },
+    (error: Response) => {
+      if (error.status === 400) {
+        alert('Bad request');
+        // this.form.setErrors(error);
+      } else {
+        alert('An unexpected error occurred');
+        console.log(error);
+      }
     });
   }
 
   updatePost(post) {
-    this.postService.updatePost(post).subscribe(response => {
+    this.postService.updatePost(post).subscribe(
+    response => {
       console.log(response);
-    }, error => {
+    },
+    error => {
       alert('An unexpected error occurred');
       console.log(error);
     });
   }
 
   deletePost(post) {
-    this.postService.deletePost(post.id).subscribe(response => {
+    this.postService.deletePost(post.id).subscribe(
+    response => {
       let index = this.posts.indexOf(post);
       this.posts.splice(index, 1);
-    }, error => {
-      alert('An unexpected error occurred');
-      console.log(error);
+    },
+    (error: Response) => {
+      if (error.status === 404) {
+        alert('This post has already been deleted');
+      } else {
+        alert('An unexpected error occurred');
+        console.log(error);
+      }
     });
   }
 }
