089e440ffefb185fd40648437659b84276c21e82
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-04 20:23:10 +0100

    136. The switchMap Operator

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GithubFollowersService } from '../services/github-followers.service';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'github-followers',
@@ -20,19 +21,21 @@ export class GithubFollowersComponent implements OnInit {
     combineLatest([
       this.route.paramMap,
       this.route.queryParamMap
-    ]).subscribe(combined => {
-      // We don't have an id here, it's just an example
-      // let id = combined[0].get('id');
-      // console.log(id);
+    ]).pipe(
+      switchMap(combined => {
+        // We don't have an id here, it's just an example
+        // let id = combined[0].get('id');
+        // console.log(id);
 
-      let page = combined[1].get('page');
-      console.log(page);
+        let page = combined[1].get('page');
+        console.log(page);
 
-      // Real world example using id and page
-      // this.service.getAll({ id: id, page: page });
-      this.service.getAll().subscribe(
-        followers => this.followers = followers
-      );
+        // Real world example using id and page
+        // return this.service.getAll({ id: id, page: page });
+        return this.service.getAll();
+      })
+    ).subscribe(followers => {
+      this.followers = followers;
     });
   }
 }
