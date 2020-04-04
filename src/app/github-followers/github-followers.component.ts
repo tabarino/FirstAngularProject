e9d8814894963204f8b138bb84e0b9e5f0a3004d
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-04 19:27:46 +0100

    134. Query Parameters

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubFollowersService } from '../services/github-followers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'github-followers',
@@ -9,9 +10,16 @@ import { GithubFollowersService } from '../services/github-followers.service';
 export class GithubFollowersComponent implements OnInit {
   followers: any;
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService
+  ) { }
 
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe(queryParams => {
+      console.log(queryParams.get('page'));
+    });
+
     this.service.getAll().subscribe(
       followers => this.followers = followers
     );
--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -11,6 +11,12 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // The Snapshot only get the params once, if it changes in the page it won't get again
+    // So, it's better using Observables, like .subscribe
+    // Using Observables, every time the model changes Angular will trigger that change
+    let id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+
     this.route.paramMap.subscribe(params => {
       // To convert a string to a number use + before your variable
       let id = +params.get('id');
