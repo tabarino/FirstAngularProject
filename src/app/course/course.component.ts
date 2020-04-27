2043ba27948e7b9b3d756ddcb1a17ec4e5b08303
tabarino <tabarino@outlook.com>
2020-04-27 22:11:16 +0100

    179. Reading an Object

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -8,11 +8,15 @@ import {AngularFirestore} from "@angular/fire/firestore";
 })
 export class CourseComponent implements OnInit, OnDestroy {
     courses$;
+    course$;
+    authors$;
 
     constructor(private db: AngularFirestore) { }
 
     ngOnInit(): void {
         this.courses$ = this.db.collection('courses').valueChanges();
+        this.course$ = this.db.doc('courses/WFVo5FTYneWySO5FgKDP').valueChanges();
+        this.authors$ = this.db.doc('authors/4eMceyeoifDHGONOELsU').valueChanges();
     }
 
     ngOnDestroy(): void {
