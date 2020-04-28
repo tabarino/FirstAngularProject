d3964b61afc91deeb32a7c1772e7398be6eef669
tabarino <tabarino@outlook.com>
2020-04-28 22:52:58 +0100

    197. Keyframes

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import {animate, state, style, transition, trigger} from "@angular/animations";
+import {animate, keyframes, state, style, transition, trigger} from "@angular/animations";
 
 export let slide = trigger('slide', [
     transition(':enter', [
@@ -7,7 +7,19 @@ export let slide = trigger('slide', [
     ]),
     transition(':leave', [
         // linear / ease-in / ease-out / ease-in-out / cubic-bezier(.17, .67, .83, .67)
-        animate('0.5s ease-in', style({transform: 'translateX(-100%)'}))
+        // animate('0.5s ease-in', style({transform: 'translateX(-100%)'}))
+        animate('0.5s ease-out', keyframes([
+            style({
+                offset: .2,
+                opacity: 1,
+                transform: 'translateX(20px)'
+            }),
+            style({
+                offset: 1,
+                opacity: 0,
+                transform: 'translateX(-100%)'
+            })
+        ]))
     ])
 ]);
 
