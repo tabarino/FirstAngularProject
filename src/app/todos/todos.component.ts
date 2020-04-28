0c82cb1897a05e20d10d979e86e1678b1c7f1abf
tabarino <tabarino@outlook.com>
2020-04-28 23:31:47 +0100

    200. Animation Callbacks

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -43,4 +43,12 @@ export class TodosComponent implements OnInit {
         let index = this.items.indexOf(item);
         this.items.splice(index, 1);
     }
+
+    animationStarted($event) {
+        console.log($event);
+    }
+
+    animationDone($event) {
+        console.log($event);
+    }
 }
