d161c8b018ac597c83056099920518136c1fe4d2
tabarino <tabarino@outlook.com>
2020-04-29 00:29:29 +0100

    205. Working with Custom States

--- src/app/zippy-animations/zippy-animations.component.ts
+++ src/app/zippy-animations/zippy-animations.component.ts
@@ -1,9 +1,26 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
     selector: 'zippy-animations',
     templateUrl: './zippy-animations.component.html',
-    styleUrls: ['./zippy-animations.component.css']
+    styleUrls: ['./zippy-animations.component.css'],
+    animations: [
+        trigger('expandCollapse', [
+            state('collapse', style({
+                height: 0,
+                paddingTop: 0,
+                paddingBottom: 0,
+                overflow: 'hidden'
+            })),
+            transition('collapse => expanded', [
+                animate('300ms ease-out')
+            ]),
+            transition('expanded => collapse', [
+                animate('300ms ease-in')
+            ])
+        ])
+    ]
 })
 export class ZippyAnimationsComponent implements OnInit {
     @Input('title') title: string;
