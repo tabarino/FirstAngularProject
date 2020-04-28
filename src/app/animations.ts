3fa73ad2211ea4faf9fc0dc86b8a5390a2b36764
tabarino <tabarino@outlook.com>
2020-04-28 22:44:44 +0100

    196. Easings - Linear/Ease-in/Ease-out/Ease-in-out/Cubic-Bezier

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -2,16 +2,17 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
 
 export let slide = trigger('slide', [
     transition(':enter', [
-        style({ transform: 'translateX(-10px)' }),
+        style({transform: 'translateX(-10px)'}),
         animate(500)
     ]),
     transition(':leave', [
-        animate(500, style({ transform: 'translateX(-100%)' }))
+        // linear / ease-in / ease-out / ease-in-out / cubic-bezier(.17, .67, .83, .67)
+        animate('0.5s ease-in', style({transform: 'translateX(-100%)'}))
     ])
 ]);
 
 export let fade = trigger('fade', [
-    state('void', style({ opacity: 0 })),
+    state('void', style({opacity: 0})),
     transition(':enter, :leave', [ // or transition('void <=> *', [
         animate(1000)
     ])
